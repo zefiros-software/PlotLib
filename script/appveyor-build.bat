c1ccfe4a86e3d512379dc680859d95873ff1cb20
Paul Visscher <paul.pev@hotmail.com>
2016-07-20 14:50:52 +0200

    Appveyor test runner

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,3 +1,13 @@
 premake5 install-package --allow-install --allow-module
 premake5 vs2015
-msbuild plot/PlotLib.sln
+msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=Win32
+msbuild plot/PlotLib.sln /property:Configuration=Debug /property:Platform=Win32
+msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=x64
+msbuild plot/PlotLib.sln /property:Configuration=Debug /property:Platform=x64
+
+cd ../docs/images/
+../../bin/x86/plot-test.exe
+../../bin/x86/plot-testd.exe
+
+../../bin/x86_64/plot-test.exe
+../../bin/x86_64/plot-testd.exe
