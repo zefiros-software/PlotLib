15ceff18749c6d0834ce6a73084aa06c2396e056
Paul Visscher <paul.pev@hotmail.com>
2016-07-20 11:48:38 +0200

    Build script

---
+++ script/travis-build.sh
@@ -0,0 +1,16 @@
+premake5 gmake
+make config=debug_x86
+make config=debug_x86_64
+make config=release_x86
+make config=release_x86_64
+make config=coverage_x86
+make config=coverage_x86_64
+
+cd docs/images/
+../../bin/x86/plot-test
+../../bin/x86/plot-testd
+../../bin/x86/plot-testcd
+
+../../bin/x86_64/plot-test
+../../bin/x86_64/plot-testd
+../../bin/x86_64/plot-testcd
