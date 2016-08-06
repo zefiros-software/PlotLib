ae03fea653f9453b9380b02951a9b8b29d39a08b
PaulVisscher <paul.pev@hotmail.com>
2016-08-06 21:16:29 +0200

    Export define properly

--- .build.lua
+++ .build.lua
@@ -2,7 +2,9 @@
 project "PlotLib"
 
     if zpm.option( "HeaderOnly" ) then
-        defines "PLOTLIB_NO_HEADER_ONLY"     
+        zpm.export [[
+        defines "PLOTLIB_NO_HEADER_ONLY"   
+        ]]  
         files "plot/src/**.cpp"   
         kind "StaticLib"
     else
@@ -10,11 +12,12 @@ project "PlotLib"
     end
 
     if zpm.option( "UseZPMAnaconda" ) then
-        defines "PLOTLIB_USE_ZPM_ANACONDA"
+        zpm.export [[
+            defines "PLOTLIB_USE_ZPM_ANACONDA"
+        ]]
     end
 
     zpm.export [[
         includedirs "plot/include/"
-        defines "PLOTLIB_USE_ZPM_ANACONDA"
         flags "C++11"
     ]]
