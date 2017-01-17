79b4a79e442d5eccde58f61eb15c0fd90557fbda
PaulVisscher <paul.pev@hotmail.com>
2017-01-17 21:03:50 +0100

    Modernise repo

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,19 +1,22 @@
-premake5 install-package --allow-install --allow-module  || exit /b 1
-premake5 vs2015 || exit /b 1
-msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b 1
-msbuild plot/PlotLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b 1
-msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b 1
-msbuild plot/PlotLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b 1
 
-cd test/
-premake5 vs2015 || exit /b 1
-msbuild zpm/PlotLib-ZPM.sln || exit /b 1
+if "%TYPE%" == "zpm" (    
+    cd test
+    
+    zpm install-package --allow-install --allow-module || exit /b 1
+    zpm vs2015 --allow-install || exit /b 1
 
-cd ../docs/images/ || exit /b 1
-..\..\bin\x86\plot-test.exe || exit /b 1
-..\..\bin\x86\plot-testd.exe || exit /b 1
+    msbuild zpm/MathLib-ZPM.sln || exit /b 1
 
-..\..\bin\x86_64\plot-test.exe || exit /b 1
-..\..\bin\x86_64\plot-testd.exe || exit /b 1
+    bin\x86\math-zpm-test.exe || exit /b 1
+) else (
+    zpm install-package --allow-install --allow-module || exit /b 1
+    zpm %VSTUD% --allow-install || exit /b 1
 
-..\..\test\bin\x86\plot-zpm-test.exe || exit /b 1
+    msbuild math/MathLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
+
+    if %TYPE% == "debug" (
+        bin\%ARCH%\math-testd
+    ) else (
+        bin\%ARCH%\math-test
+    )
+)
--- script/coverage.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-cd plot
-make config=coverage_x86
-cd ../
-
-./plot-testcd
-
-codecov
