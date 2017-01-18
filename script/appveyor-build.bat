82c7bd233d59cdde9d84b8a1601e965020e2d3d5
PaulVisscher <paul.pev@hotmail.com>
2017-01-18 19:49:59 +0100

    Fix appveyor paths

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -5,18 +5,18 @@ if "%TYPE%" == "zpm" (
     zpm install-package --allow-install --allow-module || exit /b 1
     zpm vs2015 --allow-install || exit /b 1
 
-    msbuild zpm/MathLib-ZPM.sln || exit /b 1
+    msbuild zpm/PlotLib-ZPM.sln || exit /b 1
 
-    bin\x86\math-zpm-test.exe || exit /b 1
+    bin\x86\plot-zpm-test.exe || exit /b 1
 ) else (
     zpm install-package --allow-install --allow-module || exit /b 1
     zpm %VSTUD% --allow-install || exit /b 1
 
-    msbuild math/MathLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
+    msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
 
     if %TYPE% == "debug" (
-        bin\%ARCH%\math-testd
+        bin\%ARCH%\plot-testd
     ) else (
-        bin\%ARCH%\math-test
+        bin\%ARCH%\plot-test
     )
 )
