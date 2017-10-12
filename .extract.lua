dcc29325d595f607f3d8a0c179d91c18a37a7adf
PaulVisscher <paul.pev@hotmail.com>
2017-10-12 10:48:28 +0200

    Remove extract script

--- .extract.lua
+++
@@ -1,7 +0,0 @@
-miniconda.install()
-miniconda.conda( "install seaborn" )
-miniconda.conda( "install -c taugspurger statsmodels=0.8.0" )
-
-miniconda.pip( "install mkdocs -U" )
-
-print("Weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
