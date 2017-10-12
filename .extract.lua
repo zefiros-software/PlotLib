26b893a42df5a98303dce90973d9e79ebc66cb35
PaulVisscher <paul.pev@hotmail.com>
2017-10-12 10:46:27 +0200

    Debug

--- .extract.lua
+++ .extract.lua
@@ -3,3 +3,5 @@ miniconda.conda( "install seaborn" )
 miniconda.conda( "install -c taugspurger statsmodels=0.8.0" )
 
 miniconda.pip( "install mkdocs -U" )
+
+print("Weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
