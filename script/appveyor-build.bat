51e06e2a517ba7957457e2c69d0a1dbead2af336
PaulVisscher <paul.pev@hotmail.com>
2017-12-28 19:16:31 +0100

    Update commands

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -2,15 +2,13 @@
 if "%TYPE%" == "zpm" (    
     cd test
     
-    zpm install-package --allow-install --allow-module || exit /b 1
-    zpm vs2015 --allow-install || exit /b 1
+    zpmd vs2015 || exit /b 1
 
     msbuild zpm/PlotLib-ZPM.sln || exit /b 1
 
     bin\x86\plot-zpm-test.exe || exit /b 1
 ) else (
-    zpm install-package --allow-install --allow-module || exit /b 1
-    zpm %VSTUD% --allow-install || exit /b 1
+    zpmd %VSTUD% --allow-install || exit /b 1
 
     msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
 
