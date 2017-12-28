7ca4fe12e344646a1b50ba5a0e2be5b7cbc2c1a4
PaulVisscher <paul.pev@hotmail.com>
2017-12-28 19:36:42 +0100

    Add ci flag

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -2,13 +2,13 @@
 if "%TYPE%" == "zpm" (    
     cd test
     
-    zpmd vs2015 || exit /b 1
+    zpmd vs2015 --ci || exit /b 1
 
     msbuild zpm/PlotLib-ZPM.sln || exit /b 1
 
     bin\x86\plot-zpm-test.exe || exit /b 1
 ) else (
-    zpmd %VSTUD% --allow-install || exit /b 1
+    zpmd %VSTUD% --ci || exit /b 1
 
     msbuild plot/PlotLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
 
