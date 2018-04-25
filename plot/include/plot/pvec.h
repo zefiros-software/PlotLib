fbc6d075a362053fe65e8b6896e8bf84218cd8dd
PaulVisscher <paul.pev@hotmail.com>
2018-04-25 21:32:28 +0200

    Update pvec.h

--- plot/include/plot/pvec.h
+++ plot/include/plot/pvec.h
@@ -70,7 +70,7 @@ public:
 
     template< typename tT >
     PVec(const tT &data)
-        : mData(arma::conv_to< std::vector<double>>::from(data.row))
+        : mData(arma::conv_to< std::vector<double>>::from(data))
     {
     }
 
